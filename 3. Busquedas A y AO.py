de38830975f7c0e535bfbbec0781be3a01f3051e
ErickArambula <eric.alejandro@hotmail.com>
2023-10-22 22:01:55 -0600

    2.0

---
+++ 3. Busquedas A y AO.py
@@ -0,0 +1,94 @@
+import heapq
+
+class Grafo:
+    def __init__(self):
+        self.grafo = {}
+
+    def agregar_vertice(self, vertice):
+        if vertice not in self.grafo:
+            self.grafo[vertice] = []
+
+    def agregar_arista(self, vertice1, vertice2, costo):
+        self.grafo[vertice1].append((vertice2, costo))
+        self.grafo[vertice2].append((vertice1, costo))
+
+def a_estrella(grafo, inicio, objetivo, heuristica):
+    cola_prioridad = [(0, inicio)]  # Cola de prioridad para mantener el costo acumulado y el nodo.
+    costo_acumulado = {inicio: 0}  # Diccionario para mantener el costo acumulado desde el inicio.
+    camino = {}  # Diccionario para registrar el camino desde el inicio.
+
+    while cola_prioridad:
+        costo_actual, nodo_actual = heapq.heappop(cola_prioridad)
+
+        if nodo_actual == objetivo:
+            # Reconstruir el camino desde el objetivo hasta el inicio.
+            camino_recuperado = []
+            while nodo_actual is not None:
+                camino_recuperado.insert(0, nodo_actual)
+                nodo_actual = camino.get(nodo_actual, None)
+            return camino_recuperado
+
+        for vecino, costo_arista in grafo.grafo[nodo_actual]:
+            nuevo_costo = costo_acumulado[nodo_actual] + costo_arista
+            if vecino not in costo_acumulado or nuevo_costo < costo_acumulado[vecino]:
+                costo_acumulado[vecino] = nuevo_costo
+                heuristico = nuevo_costo + heuristica[vecino]
+                heapq.heappush(cola_prioridad, (heuristico, vecino))
+                camino[vecino] = nodo_actual
+
+    return None  # No se encontró un camino al objetivo
+
+def ao_estrella(grafo, inicio, objetivo, heuristica):
+    cola_prioridad = [(0, inicio)]  # Cola de prioridad para mantener el costo acumulado y el nodo.
+    costo_acumulado = {inicio: 0}  # Diccionario para mantener el costo acumulado desde el inicio.
+    camino = {}  # Diccionario para registrar el camino desde el inicio.
+
+    while cola_prioridad:
+        costo_actual, nodo_actual = heapq.heappop(cola_prioridad)
+
+        if nodo_actual == objetivo:
+            # Reconstruir el camino desde el objetivo hasta el inicio.
+            camino_recuperado = []
+            while nodo_actual is not None:
+                camino_recuperado.insert(0, nodo_actual)
+                nodo_actual = camino.get(nodo_actual, None)
+            return camino_recuperado
+
+        for vecino, costo_arista in grafo.grafo[nodo_actual]:
+            nuevo_costo = costo_acumulado[nodo_actual] + costo_arista
+            if vecino not in costo_acumulado or nuevo_costo < costo_acumulado[vecino]:
+                costo_acumulado[vecino] = nuevo_costo
+                heuristico = nuevo_costo + heuristica[vecino]
+                heapq.heappush(cola_prioridad, (heuristico, vecino))
+                camino[vecino] = nodo_actual
+
+    return None  # No se encontró un camino al objetivo
+
+# Ejemplo de uso:
+grafo_ejemplo = Grafo()
+grafo_ejemplo.agregar_vertice('A')
+grafo_ejemplo.agregar_vertice('B')
+grafo_ejemplo.agregar_vertice('C')
+grafo_ejemplo.agregar_vertice('D')
+grafo_ejemplo.agregar_arista('A', 'B', 4)
+grafo_ejemplo.agregar_arista('A', 'C', 2)
+grafo_ejemplo.agregar_arista('B', 'D', 5)
+grafo_ejemplo.agregar_arista('C', 'D', 1)
+
+inicio = 'A'
+objetivo = 'D'
+heuristica = {'A': 7, 'B': 3, 'C': 1, 'D': 0}  # Valores heurísticos para cada nodo
+
+print("Usando A*:")
+resultado_a_estrella = a_estrella(grafo_ejemplo, inicio, objetivo, heuristica)
+if resultado_a_estrella:
+    print(f"Camino más corto de {inicio} a {objetivo} usando A*: {resultado_a_estrella}")
+else:
+    print(f"No se encontró un camino de {inicio} a {objetivo} usando A*")
+
+print("\nUsando AO*:")
+resultado_ao_estrella = ao_estrella(grafo_ejemplo, inicio, objetivo, heuristica)
+if resultado_ao_estrella:
+    print(f"Camino más corto de {inicio} a {objetivo} usando AO*: {resultado_ao_estrella}")
+else:
+    print(f"No se encontró un camino de {inicio} a {objetivo} usando AO*")
